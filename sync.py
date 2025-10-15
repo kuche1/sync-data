7abaf769bea36b9c64b06cbf12f0e39924e5eef6
kuche1 <znamsiparolata4esno@abv.bg>
2025-10-15 14:48:44 +0300

    update

--- sync.py
+++ sync.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import warnings
 from datetime import datetime
 import os
+import subprocess
 
 def node_delete(node: Path) -> None:
     now = datetime.now()
@@ -25,17 +26,16 @@ def node_symlink(real_file: Path, symlink_to_create: Path) -> None:
     print(f'Symlinking `{symlink_to_create}` -> `{real_file}`')
     symlink_to_create.symlink_to(real_file) # TODO: but what if it is a nested subdirectory ?
 
-def main(config_toml: str) -> None:
-    config_toml = Path(config_toml)
+def sudo_copy_file(source: Path, destination: Path) -> None:
+    print(f'Sudo copying `{source}` -> `{destination}`')
+    subprocess.run(['sudo', 'cp', source, destination], check=True)
 
-    with config_toml.open("rb") as f:
-        config = tomllib.load(f)
-    
-    sync_folder = config_toml.parent / config.pop('sync-folder')
+def sudo_service_start_enable(name: str) -> None:
+    subprocess.run(['sudo', 'systemctl', 'enable', '--now', name], check=True)
+
+def handle_sync_folder(config_toml_parent: Path, config: dict) -> None:
+    sync_folder = config_toml_parent / config.pop('sync-folder')
     symlinks = config.pop('symlinks') # TODO: need to check if there are any additional nodes in the sync folder that are missing here
-
-    if len(config) != 0:
-        warnings.warn(f'unknown config items: {config}')
     
     for relative_path in symlinks:
         sync_folder_node = sync_folder / relative_path
@@ -45,6 +45,43 @@ def main(config_toml: str) -> None:
             node_symlink(sync_folder_node, home_folder_node)
         else:
             warnings.warn(f'node does not exist: {sync_folder_node}')
+
+def handle_services(config_toml_parent: Path, config: dict) -> None:
+    key = "services-folder"
+
+    if key not in config:
+        return
+
+    services_folder = config_toml_parent / config.pop(key)
+
+    for dir_, folders, files in services_folder.walk():
+
+        for folder in folders:
+            path = dir_ / folder
+            warnings.warn(f'folder ignored: {path}')
+
+        for file in files:
+            path_repo = dir_ / file
+            path_system = Path('/etc/systemd/system') / file
+
+            if path_system.exists():
+                warnings.warn(f'service already exists: {path_system}')
+                continue
+
+            sudo_copy_file(path_repo, path_system)
+            sudo_service_start_enable(path_system.stem)
+
+def main(config_toml: str) -> None:
+    config_toml = Path(config_toml)
+
+    with config_toml.open("rb") as f:
+        config = tomllib.load(f)
+
+    handle_sync_folder(config_toml.parent, config)
+    handle_services(config_toml.parent, config)
+
+    if len(config) != 0:
+        warnings.warn(f'unknown config items: {config}')
 
 def parse_cmdline() -> None:
     parser = ArgumentParser()
